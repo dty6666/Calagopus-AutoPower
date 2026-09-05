b3adecfc607feb501a5172f84e6e2809c661f297
deteye <97150608+dty6666@users.noreply.github.com>
2026-09-06 02:28:18 +0800

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.keluarganaga.calagopusautopower"
-    compileSdk = 37
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.keluarganaga.calagopusautopower"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
@@ -28,13 +28,17 @@ android {
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+
     implementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
+
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
 }
